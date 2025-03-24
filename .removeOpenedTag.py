9ff3ebd3205c0f09f88937ba0ff919d68a1780f3
Jon Lawrence <jlawrence11@gmail.com>
2025-03-23 21:52:11 -0700

    New python script to remove timestamp
    Python is more universal, will use that script

---
+++ .removeOpenedTag.py
@@ -0,0 +1,12 @@
+#import os
+from pathlib import Path
+import lxml.etree as etree
+
+#python > 3.5
+files = list(Path('.').rglob('*.cod'))
+
+for file in files:
+    tree = etree.parse(file)
+    etree.strip_elements(tree, 'lastLoadedTimestamp')
+    
+    tree.write(file, encoding='utf-8', xml_declaration=True)
